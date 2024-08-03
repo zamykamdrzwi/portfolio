408cfcee1edcc37c22a903f56b35af9276165dc8
Rafal <rafdaw1337@wp.pl>
2024-08-03 12:24:02 +0200

    hamburger menu

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { ref } from "vue";
 import { RouterLink } from 'vue-router';
+import HamburgerMenu from "@/components/layouts/HamburgerMenu.vue";
 
+const isHidden = ref(true);
+const hamburger = ref(null);
+
+const toggleMenu = () => {
+  void hamburger.offsetWidth;
+  isHidden.value = !isHidden.value;
+};
 </script>
 
 <template>
@@ -13,6 +21,15 @@ import { RouterLink } from 'vue-router';
       <RouterLink to="/doswiadczenie">Doświadczenie</RouterLink>
     </nav>
     <div class="header__layout"></div>
+    <div class="header__burger">
+      <hamburger-menu @click="toggleMenu"/>
+    </div>
+    <div :class="[ 'header__phone', { 'show' : !isHidden } ]" ref="hamburger">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/projekty">Projekty</RouterLink>
+      <RouterLink to="/doswiadczenie">Doświadczenie</RouterLink>
+    </div>
   </section>
 </template>
 
@@ -25,10 +42,18 @@ import { RouterLink } from 'vue-router';
   margin-top: 15px;
   z-index: 999;
 
+  @media(max-width: 830px) {
+    justify-content: flex-end;
+  }
+
   &__menu {
     display: flex;
     gap: 20px;
     font-size: 18px;
+
+    @media(max-width: 830px) {
+      display: none;
+    }
 
     & a {
       color: $color-tertiary;
@@ -45,9 +70,35 @@ import { RouterLink } from 'vue-router';
   &__seccond {
     display: flex;
   }
-}
 
-.hidden {
-  display: none;
+  &__burger {
+    display: none;
+    margin-right: 18px;
+
+    @media(max-width: 830px) {
+      display: flex;
+    }
+  }
+
+  &__phone {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    position: fixed;
+    top: 60px;
+    right: -150px;
+    background-color: rgba(0, 0, 0, 0.2);
+    transition: right 0.5s ease-in-out;
+
+    & a {
+      color: $color-tertiary;
+      text-decoration: none;
+      padding: 5px 10px;
+    }
+  }
+
+  &__phone.show {
+    right: 9px;
+  }
 }
 </style>
---
+++ src/components/layouts/HamburgerMenu.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+const toggle = () => {
+  isOpen.value = !isOpen.value;
+}
+</script>
+
+<template>
+  <div class="hamburger-menu" @click="toggle">
+    <div :class="['item1', 'hamburger-menu__line', { 'open' : isOpen }]"></div>
+    <div :class="['item2', 'hamburger-menu__line', { 'open' : isOpen }]"></div>
+    <div :class="['item3', 'hamburger-menu__line', { 'open' : isOpen }]"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.hamburger-menu {
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+
+  &__line {
+    width: 30px;
+    height: 3px;
+    background-color: $color-tertiary;
+    border-radius: 10px;
+    transition: all 0.3s ease;
+  }
+}
+
+.item1.open {
+  transform: rotate(45deg) translate(9px, 5px);
+}
+
+.item2.open {
+  opacity: 0;
+}
+
+.item3.open {
+  transform: rotate(-45deg) translate(9px, -5px);
+}
+</style>
